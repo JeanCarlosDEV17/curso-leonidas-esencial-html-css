7b1b549c4c2f8627927225cc0f0cfe83d40be6ea
Jean Carlos DEV <jeancarlosrecio@hotmail.com>
2021-08-15 22:52:54 -0500

    finalizado los estilos con el modelo fisico

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 //Permite tener solo abierto un elemento details dentro del documento html de nuestra pagina web
 
-
-
 const $detailList = document.querySelectorAll("details");
 $detailList.forEach(($details) => {
    $details.querySelector("summary").addEventListener("click", expand);
